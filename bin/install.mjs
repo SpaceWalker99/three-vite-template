14e6db12c76ea10963d820aba30536c3af7770a3
SpaceWalker99 <cao.lei1031@gmail.com>
2024-02-09 00:29:03 +0800

    fix: fix ora issue

--- bin/install.mjs
+++ bin/install.mjs
@@ -4,7 +4,7 @@ import cp from "child_process";
 import path from "path";
 import fs, { existsSync, mkdirSync } from "fs";
 // cli spinners
-import ora from "ora";
+// import ora from "ora";
 
 // convert libs to promises
 const exec = promisify(cp.exec);
@@ -34,12 +34,12 @@ if (fs.existsSync(projectPath)) {
 }
 
 try {
-  const gitSpinner = ora("Downloading files...").start();
+  // const gitSpinner = ora("Downloading files...").start();
   // clone the repo into the project folder -> creates the new boilerplate
   await exec(`git clone --depth 1 ${git_repo} ${projectPath} --quiet`);
-  gitSpinner.succeed();
+  // gitSpinner.succeed();
 
-  const cleanSpinner = ora("Removing useless files").start();
+  // const cleanSpinner = ora("Removing useless files").start();
   // remove my git history
   const rmGit = rm(path.join(projectPath, ".git"), {
     recursive: true,
@@ -54,12 +54,12 @@ try {
 
   process.chdir(projectPath);
   // remove the packages needed for cli
-  await exec("npm uninstall ora cli-spinners");
-  cleanSpinner.succeed();
+  // await exec("npm uninstall ora cli-spinners");
+  // cleanSpinner.succeed();
 
-  const npmSpinner = ora("Installing dependencies...").start();
+  // const npmSpinner = ora("Installing dependencies...").start();
   await exec("npm install");
-  npmSpinner.succeed();
+  // npmSpinner.succeed();
 
   console.log("The installation is done!");
   console.log("You can now run your app with:");
